a7f8e5db32cedeee3851a6b59c7f4ae981e27741
Thomas Leonard <talex5@gmail.com>
2009-03-22 08:48:22 +0000

    Added 'clean' command
    Deletes the build and dist directories.

---
+++ clean.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2009, Thomas Leonard
+# See http://0install.net/0compile.html
+
+import __main__, shutil, os
+
+from support import *
+
+def do_clean(args):
+	"""clean"""
+	if args:
+		raise __main__.UsageError()
+
+	buildenv = BuildEnv()
+
+	for x in ['build', buildenv.distdir]:
+		if os.path.exists(x):
+			print "Removing '%s'" % os.path.basename(x)
+			shutil.rmtree(x)
+
+__main__.commands.append(do_clean)
