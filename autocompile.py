de2b10281edce5bc4598a1cb1a0b65c288ce79fd
Thomas Leonard <talex5@gmail.com>
2009-04-12 14:41:20 +0100

    Make autocompile actually compile and register the programs

--- autocompile.py
+++ autocompile.py
@@ -1,11 +1,13 @@
 # Copyright (C) 2009, Thomas Leonard
 # See http://0install.net/0compile.html
 
-import sys, os, __main__, tempfile
+import sys, os, __main__, tempfile, shutil, subprocess
+from xml.dom import minidom
 
 from zeroinstall import SafeException
-from zeroinstall.injector import arch, handler, policy, model, iface_cache
-from zeroinstall.support import tasks
+from zeroinstall.injector import arch, handler, policy, model, iface_cache, selections, namespaces, writer, reader
+from zeroinstall.zerostore import manifest
+from zeroinstall.support import tasks, basedir
 
 from support import BuildEnv
 
@@ -101,6 +103,83 @@ def print_details(solver):
 			print '%s (%s) : %s' % (impl.get_version(), impl.arch or '*-*', note or 'OK')
 	print "\nEnd details"
 
+def compile_and_register(policy):
+	local_feed_dir = basedir.save_config_path('0install.net', '0compile', 'builds', model._pretty_escape(policy.root))
+	s = selections.Selections(policy)
+
+	buildenv = BuildEnv(need_config = False)
+	buildenv.config.set('compile', 'interface', policy.root)
+	buildenv.config.set('compile', 'selections', 'selections.xml')
+
+	version = s.selections[policy.root].version
+	local_feed = os.path.join(local_feed_dir, '%s-%s-%s.xml' % (buildenv.iface_name, version, arch._uname[-1]))
+	if os.path.exists(local_feed):
+		raise SafeException("Build metadata file '%s' already exists!" % local_feed)
+
+	tmpdir = tempfile.mkdtemp(prefix = '0compile-')
+	try:
+		os.chdir(tmpdir)
+
+		# Write configuration for build...
+
+		buildenv.save()
+
+		sel_file = open('selections.xml', 'w')
+		try:
+			doc = s.toDOM()
+			doc.writexml(sel_file)
+			sel_file.write('\n')
+		finally:
+			sel_file.close()
+
+		# Do the build...
+
+		subprocess.check_call([sys.executable, sys.argv[0], 'build'])
+
+		# Register the result...
+
+		alg = manifest.get_algorithm('sha1new')
+		digest = alg.new_digest()
+		lines = []
+		for line in alg.generate_manifest(buildenv.distdir):
+			line += '\n'
+			digest.update(line)
+			lines.append(line)
+		actual_digest = alg.getID(digest)
+
+		local_feed_file = file(local_feed, 'w')
+		try:
+			dom = minidom.parse(buildenv.local_iface_file)
+			impl, = dom.getElementsByTagNameNS(namespaces.XMLNS_IFACE, 'implementation')
+			impl.setAttribute('id', actual_digest)
+			dom.writexml(local_feed_file)
+			local_feed_file.write('\n')
+		finally:
+			local_feed_file.close()
+
+		print "Implementation metadata written to %s" % local_feed
+
+		print "Storing build in cache..."
+		policy.solver.iface_cache.stores.add_dir_to_cache(actual_digest, buildenv.distdir)
+
+		print "Registering feed..."
+		iface = policy.solver.iface_cache.get_interface(policy.root)
+		feed = iface.get_feed(local_feed)
+		if feed:
+			print "WARNING: feed %s already registered!" % local_feed
+		else:
+			iface.extra_feeds.append(model.Feed(local_feed, impl.getAttribute('arch'), user_override = True))
+		writer.save_interface(iface)
+
+		# We might have cached an old version
+		new_feed = policy.solver.iface_cache.get_interface(local_feed)
+		reader.update_from_cache(new_feed)
+	except:
+		print "\nBuild failed: leaving build directory %s for inspection...\n" % tmpdir
+		raise
+	else:
+		shutil.rmtree(tmpdir)
+
 def do_autocompile(args):
 	"""autocompile URI"""
 	if len(args) != 1:
@@ -121,28 +200,35 @@ def do_autocompile(args):
 		#p.target_arch = arch.Architecture(os_ranks = {'FreeBSD': 0, None: 1}, machine_ranks = {'i386': 0, None: 1, 'newbuild': 2})
 
 		print (' %s ' % iface_uri).center(76, '=')
-		print "\nSelecting versions..."
-		solved = p.solve_with_downloads()
-		if solved:
-			yield solved
-			tasks.check(solved)
 
-		if not p.solver.ready:
-			print_details(p.solver)
-			raise SafeException("Can't find all required implementations (source or binary):\n" +
-				'\n'.join(["- %s -> %s" % (iface, p.solver.selections[iface])
-					   for iface in p.solver.selections]))
-		print "Selection done."
+		while True:
+			print "\nSelecting versions for %s..." % iface.get_name()
+			solved = p.solve_with_downloads()
+			if solved:
+				yield solved
+				tasks.check(solved)
 
-		print "\nPlan:\n"
-		pretty_print_plan(p.solver, p.root)
+			if not p.solver.ready:
+				print_details(p.solver)
+				raise SafeException("Can't find all required implementations (source or binary):\n" +
+					'\n'.join(["- %s -> %s" % (iface, p.solver.selections[iface])
+						   for iface in p.solver.selections]))
+			print "Selection done."
 
-		for iface, impl in p.solver.selections.iteritems():
-			if impl.id.startswith('0compile='):
-				print
-				build = recursive_build(iface.uri, impl.get_version())
-				yield build
-				tasks.check(build)
+			print "\nPlan:\n"
+			pretty_print_plan(p.solver, p.root)
+			print
+
+			for dep_iface, dep_impl in p.solver.selections.iteritems():
+				if dep_impl.id.startswith('0compile='):
+					build = recursive_build(dep_iface.uri, dep_impl.get_version())
+					yield build
+					tasks.check(build)
+					break	# Try again with that dependency built...
+			else:
+				print "No dependencies need compiling... compile %s itself..." % iface.get_name()
+				compile_and_register(p)
+				return
 
 	h.wait_for_blocker(recursive_build(iface_uri))
 
