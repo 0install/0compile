0f187e8ec71ebbd5f0059a700fde03836f5c8f18
Thomas Leonard <talex5@gmail.com>
2009-04-19 12:12:47 +0100

    Added publish --target-feed option
    This is useful for build scripts, since the caller doesn't have to guess
    what the new feed will be called.

--- publish.py
+++ publish.py
@@ -4,17 +4,24 @@
 import sys, os, __main__
 from logging import info
 from xml.dom import minidom
+from optparse import OptionParser
 
 from support import *
 
 def do_publish(args):
 	"""publish [ DOWNLOAD-BASE-URL ]"""
+
+	parser = OptionParser(usage="usage: %prog publish [options] [ DOWNLOAD-BASE-URL ]")
+
+	parser.add_option('', "--target-feed", help="name of output feed file to create", metavar='FILE')
+	(options, args2) = parser.parse_args(args)
+
 	buildenv = BuildEnv()
-	if len(args) == 0:
+	if len(args2) == 0:
 		if not buildenv.download_base_url:
 			raise SafeException("No download base set. Give the URL for a remote directory.")
-	elif len(args) == 1:
-		buildenv.config.set('compile', 'download-base-url', args[0])
+	elif len(args2) == 1:
+		buildenv.config.set('compile', 'download-base-url', args2[0])
 		buildenv.save()
 
 	info("Using download base URL: %s", buildenv.download_base_url)
@@ -34,17 +41,22 @@ def do_publish(args):
 	archive.add(distdir, buildenv.archive_stem)
 	archive.close()
 
+	target_feed = options.target_feed or buildenv.local_download_iface
+
 	download_url = os.path.join(buildenv.download_base_url, archive_name)
-	shutil.copyfile(buildenv.local_iface_file, buildenv.local_download_iface)
+	shutil.copyfile(buildenv.local_iface_file, target_feed)
 	
 	spawn_and_check(find_in_path('0launch'),
-		['http://0install.net/2006/interfaces/0publish', buildenv.local_download_iface,
+		['http://0install.net/2006/interfaces/0publish', target_feed,
 		'--archive-url', download_url,
 		'--archive-extract', buildenv.archive_stem])
 
-	print "Now upload '%s' as:\n%s\n" % (archive_name, download_url)
+	if options.target_feed is None:
+		# If --target-feed is used this is probably a script, so don't print
+		# out hints.
+		print "Now upload '%s' as:\n%s\n" % (archive_name, download_url)
 
-	print "Once uploaded, you can download and run with:"
-	print "$ 0launch %s" % buildenv.local_download_iface
+		print "Once uploaded, you can download and run with:"
+		print "$ 0launch %s" % target_feed
 
 __main__.commands.append(do_publish)
