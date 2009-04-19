fffaa97f5587903c9c999d8d11d43a61aece9c45
Thomas Leonard <talex5@gmail.com>
2009-04-19 12:01:02 +0100

    Revert "Added 'autopublish' command"
    This command was intended for 0release, but that didn't use it in the
    end. The name is confusingly similar to autocompile, but doesn't work
    the same way.
    
    This reverts commit d82afa3d47b8449e59f3d006331926054e80bf1d.

--- publish.py
+++ publish.py
@@ -7,33 +7,15 @@ from xml.dom import minidom
 
 from support import *
 
-def ensure_download_base(buildenv, args):
+def do_publish(args):
+	"""publish [ DOWNLOAD-BASE-URL ]"""
+	buildenv = BuildEnv()
 	if len(args) == 0:
 		if not buildenv.download_base_url:
 			raise SafeException("No download base set. Give the URL for a remote directory.")
 	elif len(args) == 1:
 		buildenv.config.set('compile', 'download-base-url', args[0])
 		buildenv.save()
-	else:
-		raise __main__.UsageError()
-
-def do_autopublish(args):
-	"""autopublish [ DOWNLOAD-BASE-URL ]"""
-	import clean
-	import build
-
-	buildenv = BuildEnv()
-	ensure_download_base(buildenv, args)
-
-	clean.do_clean([])
-	build.do_build([])
-	do_publish([])
-	clean.do_clean([])
-
-def do_publish(args):
-	"""publish [ DOWNLOAD-BASE-URL ]"""
-	buildenv = BuildEnv()
-	ensure_download_base(buildenv, args)
 
 	info("Using download base URL: %s", buildenv.download_base_url)
 
@@ -65,4 +47,4 @@ def do_publish(args):
 	print "Once uploaded, you can download and run with:"
 	print "$ 0launch %s" % buildenv.local_download_iface
 
-__main__.commands += [do_autopublish, do_publish]
+__main__.commands.append(do_publish)
